f3bc88555a803bb1269a05d86128b9a6d9d18ebe
John Else <john.else@citrix.com>
2013-10-03 11:40:28 +0100

    Add comments
    Signed-off-by: John Else <john.else@citrix.com>

--- get_device_info.c
+++ get_device_info.c
@@ -7,6 +7,7 @@ int main(int argc, char** argv) {
     unsigned int device_count, device_index, temp;
     nvmlDevice_t device;
 
+    // Initialise the library.
     result = nvmlInit();
     if(NVML_SUCCESS != result) {
         printf("Failed to initialise: %s\n", nvmlErrorString(result));
@@ -14,6 +15,7 @@ int main(int argc, char** argv) {
     };
     printf("nvmlInit successful\n");
 
+    // Get the number of installed devices.
     result = nvmlDeviceGetCount(&device_count);
     if(NVML_SUCCESS != result) {
         printf("Failed to get device count: %s\n", nvmlErrorString(result));
@@ -21,9 +23,12 @@ int main(int argc, char** argv) {
     };
     printf("found %u devices\n", device_count);
 
+    // Iterate through the devices.
     for(device_index = 0; device_index < device_count; device_index++) {
+        // Get the device's handle.
         result = nvmlDeviceGetHandleByIndex(device_index, &device);
         if(NVML_SUCCESS == result) {
+            // Get the device's temperature.
             result = nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, &temp);
             if(NVML_SUCCESS == result) {
                 printf("device %u has temperature %uC\n", device_index, temp);
@@ -37,6 +42,7 @@ int main(int argc, char** argv) {
         };
     };
 
+    // Shutdown the library.
     result = nvmlShutdown();
     if(NVML_SUCCESS != result)
         printf("Failed to shutdown: %s\n", nvmlErrorString(result));
@@ -44,6 +50,7 @@ int main(int argc, char** argv) {
     exit(0);
 
 Error:
+    // Handle any unrecoverable errors.
     result = nvmlShutdown();
     if(NVML_SUCCESS != result)
         printf("Failed to shutdown: %s\n", nvmlErrorString(result));
