68bf054b7b56b36c642222be173094aab395ed42
John Else <john.else@citrix.com>
2013-10-17 23:15:05 +0100

    Load some symbols
    Signed-off-by: John Else <john.else@citrix.com>

--- ml_dlopen/ml_dlopen_stubs.c
+++ ml_dlopen/ml_dlopen_stubs.c
@@ -14,6 +14,9 @@
 
 typedef struct nvmlInterface {
     void* handle;
+    char* (*errorString)(nvmlReturn_t);
+    nvmlReturn_t (*init)(void);
+    nvmlReturn_t (*shutdown)(void);
 } nvmlInterface;
 
 CAMLprim value stub_nvml_open(value unit)
@@ -23,15 +26,35 @@ CAMLprim value stub_nvml_open(value unit)
 
     void *handle;
     nvmlInterface *interface;
+
     interface = malloc(sizeof(nvmlInterface));
 
+    // Open the library.
     handle = dlopen("libnvidia-ml.so.1", RTLD_LAZY);
     if (!handle) {
         goto Error;
     }
     interface->handle = handle;
-    ml_interface = (value)interface;
 
+    // Load nvmlErrorString.
+    interface->errorString = dlsym(handle, "nvmlErrorString");
+    if (!interface->errorString) {
+        goto Error;
+    }
+
+    // Load nvmlInit.
+    interface->init = dlsym(handle, "nvmlInit");
+    if (!interface->init) {
+        goto Error;
+    }
+
+    // Load nvmlShutdown.
+    interface->shutdown = dlsym(handle, "nvmlShutdown");
+    if (!interface->shutdown) {
+        goto Error;
+    }
+
+    ml_interface = (value)interface;
     CAMLreturn(ml_interface);
 
 Error:
