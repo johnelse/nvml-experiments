4ffc1a291d39d10d5a305d99738bf1ee3b25ae7c
John Else <john.else@citrix.com>
2013-10-18 13:10:53 +0100

    Use the new bindings in ml_dlopen
    Signed-off-by: John Else <john.else@citrix.com>

--- ml_dlopen/ml_dlopen.ml
+++ ml_dlopen/ml_dlopen.ml
@@ -28,5 +28,29 @@ end
 let () =
 	let interface = Nvml.library_open () in
 	Nvml.init interface;
+	let count = Nvml.device_get_count interface in
+	for index = 0 to (count - 1) do
+		Printf.printf "----- Device %d -----\n" index;
+		let device = Nvml.device_get_handle_by_index interface index in
+		let temp = Nvml.device_get_temperature interface device in
+		Printf.printf "temperature = %dC\n" temp;
+		begin
+			try
+				let power_usage = Nvml.device_get_power_usage interface device in
+				Printf.printf "power usage = %dmW\n" power_usage
+			with _ ->
+				Printf.printf "power usage lookup not supported\n"
+		end;
+		begin
+			try
+				let utilization = Nvml.device_get_utilization_rates interface device in
+				Printf.printf
+					"utilization = %d%% GPU, %d%% memory\n"
+					utilization.Nvml.gpu
+					utilization.Nvml.memory
+			with _ ->
+				Printf.printf "utilization lookup not supported\n"
+		end
+	done;
 	Nvml.shutdown interface;
 	Nvml.library_close interface
